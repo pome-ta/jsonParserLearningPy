e8d36475be6549bae00986b947915fe7fef38359
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-05-02 11:57:46 +0900

    up

--- sandbox/split_str02.py
+++ sandbox/split_str02.py
@@ -1,6 +1,5 @@
 json_data = '[{"nam e": "Taro", "age": 14, "check": true}, {"name": "Jiro", "age": 23, "check": false}, {"name": "Tom", "age": 16, "check": false}, {"name": null, "age": 14, "check": null}]'
 
-json_chr_list = list(json_data)
 
 def seek_quotation_index(crop_list):
   for n, string in enumerate(crop_list):
@@ -12,9 +11,75 @@ def seek_quotation_index(crop_list):
 
 def seek_number_index(crop_list):
   n = crop_list.index(',')
-  for n, string in enumerate(crop_list):
-    if string == '"':
-      if crop_list[n - 1] == '\\':
-        continue
-      return n
+  return n
+
+
+json_chr_list = list(json_data)
+str_length = len(json_chr_list)
+tokens = []
+symbols = ['[', ']', '{', '}', ':', ',']
+
+str_value = ''
+num_value = ''
+
+index = 0
+
+for _ in range(str_length):
+  if index >= str_length:
+    break
+  element = json_chr_list[index]
+
+  if element.isspace():
+    index += 1
+    continue
+
+  if element in symbols:
+    tokens.append(element)
+    index += 1
+    continue
+
+  if element == 't':
+    cut_top_list = json_chr_list[index:]
+    tokens.append(''.join(cut_top_list[:4]))
+    index += 4
+    continue
+
+  if element == 'f':
+    cut_top_list = json_chr_list[index:]
+    tokens.append(''.join(cut_top_list[:5]))
+    index += 5
+    continue
+
+  if element == 'n':
+    cut_top_list = json_chr_list[index:]
+    tokens.append(''.join(cut_top_list[:4]))
+    index += 4
+    continue
+
+  # string
+  if element == '"':
+    str_value += element
+    index += 1
+    cut_top_list = json_chr_list[index:]
+    step_index = seek_quotation_index(cut_top_list) + 1
+    str_value += ''.join(cut_top_list[:step_index])
+    tokens.append(str_value)
+    str_value = ''
+    index += step_index
+    continue
+
+  # number
+  if element:
+    num_value += element
+    index += 1
+    cut_top_list = json_chr_list[index:]
+    step_index = seek_number_index(cut_top_list)
+    num_value += ''.join(cut_top_list[:step_index])
+    tokens.append(num_value)
+    num_value = ''
+    index += step_index
+    continue
+
+  print(f'{element: element}')
+  index += 1
 
