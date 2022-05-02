7ab8e6bc862cf4d5400ef74812e759efd5d6a15d
pome-ta <pometarou93@gmail.com>
2022-05-02 22:50:57 +0900

    quotation

--- sandbox/split_str02.py
+++ sandbox/split_str02.py
@@ -1,19 +1,9 @@
 from pathlib import Path
 
-json_path = Path('./sample01.json')
+# --- テストデータ
 
-json_data = '''{
-   "number":123,
-   "boolean":true,
-   "string":"togatoga",
-   "object":{
-      "number":2E10
-   }
-}
-'''
-
-# xxx: エスケープシーケンス対応... 1行読み取り？
-json_data = '''{
+# todo: エスケープシーケンス指定時は、raw文字列指定
+json_data = r'''{
         "k0": "\" \\ \/ \b \f \n \r \t",
         "key0": "screwsat",
         "key1": 10,
@@ -28,22 +18,25 @@ json_data = '''{
         "key6": -11.0111,
         "key7": 1e-10
 }
-
 '''
 
-json_data = '[{"nam e": "Taro", "age": 14, "check": true}, {"name": "Jiro", "age": 23, "check": false}, {"name": "Tom", "age": 16, "check": false}, {"name": null, "age": 14, "check": null}]'
+# json_data = '[{"nam e": "Taro", "age": 14, "check": true}, {"name": "Jiro", "age": 23, "check": false}, {"name": "Tom", "age": 16, "check": false}, {"name": null, "age": 14, "check": null}]'
+
+
 
 
 
 def get_string_step(tail_list):
   quotation_flag = False
   for n, string in enumerate(tail_list):
-    
     if string == '"':
+      # todo: 先頭の`"` だと、最後尾index を取得してしまうため
+      if n and tail_list[n - 1] == '\\':
+        # print(f'string: {string}')
+        # print(f'[n - 1]: {tail_list[n-1]}')
+        continue
       if quotation_flag:
         break
-      if tail_list[n - 1] == '\\':
-        continue
       quotation_flag = True
   str_value = ''.join(tail_list[:n + 1])
   return str_value, len(str_value)
@@ -58,75 +51,76 @@ def get_number_step(tail_list):
   return num_value, len(num_value)
 
 
-json_chr_list = list(json_data)
-json_raw_list = list(repr(json_data))
+def get_tokens(str_list):
+  tokens = []
+  symbols = ['[', ']', '{', '}', ':', ',']
+  str_length = len(str_list)
+  index = 0
 
-qs_s = '"a\"b"'
-for ii in qs_s:
-  print(ii)
-qr_s = r'"a\"b"'
-qp_s = repr(qs_s)
-qb_s = b'"a\"b"'
+  for _ in range(str_length):
+    if index >= str_length:
+      break
 
+    element = str_list[index]
 
+    if element.isspace():
+      index += 1
+      continue
 
-str_length = len(json_chr_list)
-tokens = []
-symbols = ['[', ']', '{', '}', ':', ',']
-index = 0
+    if element in symbols:
+      tokens.append(element)
+      index += 1
+      continue
 
-for _ in range(str_length):
-  if index >= str_length:
-    break
+    if element == 't':
+      cut_top_list = str_list[index:]
+      # xxx: `true` 確認
+      tokens.append(''.join(cut_top_list[:4]))
+      index += 4
+      continue
 
-  element = json_chr_list[index]
+    if element == 'f':
+      cut_top_list = str_list[index:]
+      # xxx: `false` 確認
+      tokens.append(''.join(cut_top_list[:5]))
+      index += 5
+      continue
 
-  if element.isspace():
+    if element == 'n':
+      cut_top_list = str_list[index:]
+      # xxx: `null` 確認
+      tokens.append(''.join(cut_top_list[:4]))
+      index += 4
+      continue
+
+    # string
+    if element == '"':
+      cut_top_list = str_list[index:]
+      str_value, step = get_string_step(cut_top_list)
+      tokens.append(str_value)
+      index += step
+      continue
+
+    # number
+    if element:
+      cut_top_list = str_list[index:]
+      num_value, step = get_number_step(cut_top_list)
+      tokens.append(num_value)
+      index += step
+      continue
+
+    # xxx: ここに来てたらエラー
+    print(f'err!element: {element}')
     index += 1
-    continue
 
-  if element in symbols:
-    tokens.append(element)
-    index += 1
-    continue
+  return tokens
 
-  if element == 't':
-    cut_top_list = json_chr_list[index:]
-    # xxx: `true` 確認
-    tokens.append(''.join(cut_top_list[:4]))
-    index += 4
-    continue
 
-  if element == 'f':
-    cut_top_list = json_chr_list[index:]
-    # xxx: `false` 確認
-    tokens.append(''.join(cut_top_list[:5]))
-    index += 5
-    continue
+if __name__ == '__main__':
+  from pprint import pprint
 
-  if element == 'n':
-    cut_top_list = json_chr_list[index:]
-    # xxx: `null` 確認
-    tokens.append(''.join(cut_top_list[:4]))
-    index += 4
-    continue
-
-  # string
-  if element == '"':
-    cut_top_list = json_chr_list[index:]
-    str_value, step = get_string_step(cut_top_list)
-    tokens.append(str_value)
-    index += step
-    continue
-
-  # number
-  if element:
-    cut_top_list = json_chr_list[index:]
-    num_value, step = get_number_step(cut_top_list)
-    tokens.append(num_value)
-    index += step
-    continue
-
-  print(f'{element: element}')
-  index += 1
-
+  json_path = Path('./sandbox/sample01.json')
+  json_data = json_path.read_text()
+  json_chr_list = list(json_data)
+  json_tokens = get_tokens(json_chr_list)
+  pprint(json_tokens)
