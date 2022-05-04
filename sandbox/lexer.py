2dda28e51544e814ab2c9094659a35e4c857ab87
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-05-04 15:14:44 +0900

    get lexer

--- sandbox/lexer.py
+++ sandbox/lexer.py
@@ -1,9 +1,8 @@
 from enum import Enum, auto
 from typing import List
 
-from tokenTypes 
+from tokenTypes import TokenKind
 from split_str02 import get_tokens
-
 
 
 class Token:
@@ -15,7 +14,9 @@ class Token:
     if not isinstance(other, Token):
       return NotImplemented
     return self.kind == other.kind and self.value == other.value
-    
+
+
+'''
     def __str__(self):
       if self.kind == TokenKind.LBRACKET:
         return '['
@@ -30,25 +31,56 @@ class Token:
       elif self.kind == TokenKind.RBRACKET:
         return ']'
       elif self.kind == TokenKind.STRING:
-        return f'{self.value}'
+        return str(self.value)
       else:
         return str(self.value)
 
+'''
+
+
+# xxx: 2回回して無駄だけど取り敢えず
 def get_lexer(token_list):
   stack = []
   for t in token_list:
     if t == '[':
-      stack.append()
-      
-      
+      stack.append(Token(TokenKind.LBRACKET, t))
+    elif t == '{':
+      stack.append(Token(TokenKind.LBRACE, t))
+    elif t == '{':
+      stack.append(Token(TokenKind.LBRACE, t))
+    elif t == ':':
+      stack.append(Token(TokenKind.COLON, t))
+    elif t == ',':
+      stack.append(Token(TokenKind.COMMA, t))
+    elif t == '}':
+      stack.append(Token(TokenKind.RBRACE, t))
+    elif t == ']':
+      stack.append(Token(TokenKind.RBRACKET, t))
+    elif t[0] == 't':
+      stack.append(Token(TokenKind.BOOLEAN, t))
+    elif t[0] == 'f':
+      stack.append(Token(TokenKind.BOOLEAN, t))
+    elif t[0] == 'n':
+      stack.append(Token(TokenKind.NULL, t))
+    elif t[0] == '"':
+      value = t[1:len(t) - 1]
+      stack.append(Token(TokenKind.STRING, value))
+    else:
+      stack.append(Token(TokenKind.NUMBER, t))
+
+  return stack
+
 
 if __name__ == '__main__':
   from pprint import pprint
   from pathlib import Path
-  tokens = []
+
   json_path = Path('./sample01.json')
   json_data = json_path.read_text(encoding='utf-8')
   json_chr_list = list(json_data)
-  json_tokens = get_tokens(json_chr_list)
-  
+  pre_tokens = get_tokens(json_chr_list)
+  json_tokens = get_lexer(pre_tokens)
+
+  for jt in json_tokens:
+    print(f'{jt.kind} ->{jt.value}')
 
