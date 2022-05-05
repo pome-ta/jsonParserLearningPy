9654f3bfdbb6c14bf6dbb9e7d77a2175c0335d47
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-05-06 00:04:05 +0900

    up

--- sandbox/tokenParse.py
+++ sandbox/tokenParse.py
@@ -3,14 +3,25 @@ from split_str02 import get_tokens
 from lexer import get_lexer
 
 
-
 def simple_parse(token_list):
-  pass
+  stack = None
+  for index in range(len(token_list)):
+    if index == 0 and (token_list[index].kind == TokenKind.LBRACKET or
+                       token_list[index].kind == TokenKind.LBRACE):
+      if token_list[index].kind == TokenKind.LBRACKET:
+        stack = list()
+        continue
+      if token_list[index].kind == TokenKind.LBRACE:
+        stack = dict()
+        continue
+    index
+
 
 def main():
   json_chr_list = list(json_data)
   pre_tokens = get_tokens(json_chr_list)
   json_tokens = get_lexer(pre_tokens)
+  simple_parse(json_tokens)
   return json_tokens
 
 
@@ -20,5 +31,6 @@ if __name__ == '__main__':
 
   json_path = Path('./sample01.json')
   json_data = json_path.read_text(encoding='utf-8')
+  json_data = '[{"nam e": "Taro", "age": 14, "check": true}, {"name": "Jiro", "age": 23, "check": false}, {"name": "Tom", "age": 16, "check": false}, {"name": null, "age": 14, "check": null}]'
   jjj = main()
 
