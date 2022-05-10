860eb581c36e8ad5a7a79a3f3abfccb68e613d9f
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-05-10 21:40:04 +0900

    obj div

--- sandbox01/parse02.py
+++ sandbox01/parse02.py
@@ -1,25 +1,15 @@
 from tokenTypes import TokenKind
 from split_str02 import get_tokens
 from lexer import get_lexer
-"""
-再度、個々でラベリングをして
-個別で対応、その後に統合する？
-key は設定完了してる
-value としての条件
-  `:` 次の`,`
-    連続している場合には、`{` か`[` が出現するはず
-  `:` の次の`}`
-インデントのグループ分けをした方がいい？
-"""
 
 
-def indent_deep(tokens):
+def set_deep(tokens):
   deep = 1
   for tkn in tokens:
-    if tkn.kind == TokenKind.LBRACKET or tkn.kind == TokenKind.LBRACE:
+    if tkn.kind in [TokenKind.LBRACE, TokenKind.LBRACKET]:
       tkn.deep = deep
       deep += 1
-    if tkn.kind == TokenKind.RBRACE or tkn.kind == TokenKind.RBRACKET:
+    if tkn.kind in [TokenKind.RBRACE, TokenKind.RBRACKET]:
       deep -= 1
       tkn.deep = deep
 
@@ -33,116 +23,104 @@ def set_keytype(tokens):
 
 def get_deep_list(tokens):
   pool = []
-  for index in range(len(tokens)):
-    if tokens[index].deep:
-      pool.append([index, tokens[index].deep])
+  for n, tkn in enumerate(tokens):
+    if tkn.deep:
+      pool.append([n, tkn.deep])
 
   search = [p for p in pool]
   stack = []
-  for i in range(len(pool)):
-    i_d = pool[i]
-    if i_d == None:
+  for p_index in range(len(pool)):
+    p_deep = pool[p_index]
+    if p_deep == None:
       continue
-    indent = pool[i][1]
-    search[i] = None
-    pool[i] = None
-    for si in range(len(pool)):
-      s_id = search[si]
-      if s_id and s_id[1] == i_d[1]:
-        stack.append([i_d[0], s_id[0], indent])
-        search[si] = None
-        pool[si] = None
+    indent = pool[p_index][1]
+    search[p_index] = None
+    pool[p_index] = None
+    for s_index in range(len(pool)):
+      s_deep = search[s_index]
+      if s_deep and s_deep[1] == p_deep[1]:
+        stack.append([p_deep[0], s_deep[0], indent])
+        search[s_index] = None
+        pool[s_index] = None
         break
   return stack
 
 
-"""
-階層ができた
-key 情報は持っている
-value を抜き出したい
-value は下階層も想定
-key: value
-key: value, key:value
-value(key:value)
-1 のkey の一つ前か、`}` まで
-"""
-
-
 def set_indent(tokens, deeps):
   for s, e, i in deeps:
-    _token = tokens[s:e + 1]
-    for tkn in _token:
+    ext_tokens = tokens[s:e + 1]
+    for tkn in ext_tokens:
       tkn.indent = i
 
 
+def get_token_list(strs):
+  str_list = list(strs)
+  tokens = get_lexer(get_tokens(str_list))
+  return tokens
+
+
 def get_dicts(tokens, indent=1):
-  key = None
+  dic_key = None
+  dic_value = None
   values = []
-  pool = {}
+  stack = {}
   colon_flag = False
   children_flag = False
   for tkn in tokens:
     if tkn.indent == indent:
       if tkn.keytype:
-        key = tkn.value
+        dic_key = tkn.value
       if tkn.kind == TokenKind.COLON:
         colon_flag = True
-      if tkn.kind == TokenKind.COMMA or tkn.kind == TokenKind.RBRACE:
+      if tkn.kind in [TokenKind.COMMA, TokenKind.RBRACE]:
+        # if tkn.kind == TokenKind.COMMA or tkn.kind == TokenKind.RBRACE:
         if children_flag:
           children_flag = False
           if values[0].kind == TokenKind.LBRACKET:
-            values = get_lists(values, indent + 1)
+            dic_value = get_lists(values, indent + 1)
           elif values[0].kind == TokenKind.LBRACE:
-            values = get_dicts(values, indent + 1)
-
-        pool.update({key: values})
-        colon_flag = False
-        key = None
+            dic_value = get_dicts(values, indent + 1)
+        stack.update({dic_key: dic_value})
+        dic_key = None
+        dic_value = None
         values = []
-
-      if colon_flag and not (tkn.kind == TokenKind.COMMA or
-                             tkn.kind == TokenKind.COLON):
-
-        values = tkn.value
-        # continue
-
+      if colon_flag and not (tkn.kind in [TokenKind.COMMA, TokenKind.COLON]):
+        # if colon_flag and not (tkn.kind == TokenKind.COMMA or tkn.kind == TokenKind.COLON):
+        dic_value = tkn.value
     else:
       values.append(tkn)
       children_flag = True
-      # continue
-  return pool
+  return stack
 
 
 def get_lists(tokens, indent=1):
-  value = None
+  list_value = None
   values = []
-  pool = []
+  stack = []
   children_flag = False
   for tkn in tokens:
     if tkn.indent == indent:
-
-      if tkn.kind == TokenKind.COMMA or tkn.kind == TokenKind.RBRACKET:
-        #print(tkn)
+      if tkn.kind in [TokenKind.COMMA, TokenKind.RBRACKET]:
+        # if tkn.kind == TokenKind.COMMA or tkn.kind == TokenKind.RBRACKET:
         if children_flag:
           children_flag = False
           if values[0].kind == TokenKind.LBRACKET:
-            value = get_lists(values, indent + 1)
+            list_value = get_lists(values, indent + 1)
           elif values[0].kind == TokenKind.LBRACE:
-            value = get_dicts(values, indent + 1)
-        pool.append(value)
-        value = None
+            list_value = get_dicts(values, indent + 1)
+        stack.append(list_value)
+        list_value = None
         values = []
-
-      if tkn.kind != TokenKind.LBRACKET or tkn.kind != TokenKind.COMMA:
-        value = tkn.value
-
+      if not (tkn.kind in [TokenKind.LBRACKET, TokenKind.COMMA]):
+        # if tkn.kind != TokenKind.LBRACKET or tkn.kind != TokenKind.COMMA:
+        list_value = tkn.value
     else:
       values.append(tkn)
       children_flag = True
-  return pool
+  return stack
 
 
-def py_parse(tokens):
+def get_obj(tokens):
   stack = None
   if tokens[0].kind == TokenKind.LBRACKET:
     stack = get_lists(tokens)
@@ -151,38 +129,25 @@ def py_parse(tokens):
   return stack
 
 
-def pre_parse(strs):
-  str_list = list(strs)
-  tokens = get_tokens(str_list)
-  lexer = get_lexer(tokens)
-  indent_deep(lexer)
-  set_keytype(lexer)
-  return lexer
-
-
-def main(strs):
-  pre = pre_parse(strs)
-  deep_list = get_deep_list(pre)
-  set_indent(pre, deep_list)
-  r = py_parse(pre)
-  #indent = max([n[2] for n in deep_list])
-
+def parse(strs):
+  token_list = get_token_list(strs)
+  set_deep(token_list)
+  set_keytype(token_list)
+  deep_list = get_deep_list(token_list)
+  set_indent(token_list, deep_list)
+  obj = get_obj(token_list)
   a = 1
-  return r
+  return obj
 
 
 if __name__ == '__main__':
   from pathlib import Path
-
+  import json
+  
   json_path = Path('./sample01.json')
   #json_path = Path('./sandbox01/sample01.json')
   json_data = json_path.read_text(encoding='utf-8')
-  #test_data = '{"hoge":"fuga","piyo":{"あ":null, "い":true, "う":false}, "foo":"baa"}'
-  test_data = '{"hoge":"fuga","piyo":{"あ":null, "い":true, "う":false}, "list":[1,2,4],"foo":"baa"}'
-  #test_data = '{"hoge":"fuga", "foo": "baa"}'
-  mmain = main(test_data)
-  #mmain = main(json_data)
-  #sample_data = {"hoge": "fuga","piyo": {"あ": "null", "い": "true","う": "false"},"foo": "baa"}
-  sample_data = {"hoge": "fuga", "foo": "baa"}
+  sample = json.loads(json_data)
+  dump = parse(json_data)
   a = 1
 
