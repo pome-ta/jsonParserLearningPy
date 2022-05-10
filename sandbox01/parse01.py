c31723b1a7f4bc1c989cf7bdf76b5e94982e4b51
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-05-10 11:53:19 +0900

    dic

--- sandbox01/parse01.py
+++ sandbox01/parse01.py
@@ -8,8 +8,19 @@ def pre_parse(strs):
   tokens = get_tokens(str_list)
   lexer = get_lexer(tokens)
   return lexer
-  
+
+
+def set_keytype(tokens):
+  for index in range(len(tokens)):
+    tokens[index].keytype = False
+    if tokens[index].kind == TokenKind.COLON:
+      tokens[index - 1].keytype = True
+
+
 def create_dicts(tokens):
+  set_keytype(tokens)
+  key = None
+  value = None
   for tkn in tokens:
     if tkn.kind == TokenKind.LBRACE:
       pass
@@ -19,9 +30,9 @@ def main(strs):
   pre = pre_parse(strs)
   create_dicts(pre)
   return None
-  
+
 
 if __name__ == '__main__':
-  test_data = '{"hoge":"fuga"}'
+  test_data = '{"hoge":"fuga", "foo":"baa"}'
   main = main(test_data)
 
