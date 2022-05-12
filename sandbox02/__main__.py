adcc102c1f540125fae297d8b0ee90f93cdbcb77
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-05-12 16:03:21 +0900

    Switch

--- sandbox02/__main__.py
+++ sandbox02/__main__.py
@@ -27,40 +27,50 @@ class Token:
     return str(self.value)
 
 
-symbols = {
-  '[': Token(TokenType.L_BRACKET, None),
-  ']': Token(TokenType.R_BRACKET, None),
-  '{': Token(TokenType.L_BRACE, None),
-  '}': Token(TokenType.R_BRACE, None),
-  ':': Token(TokenType.COLON, None),
-  ':': Token(TokenType.COMMA, None),
-}
+# xxx: 無駄？
+def get_symbol_dict():
+  return {
+    '[': Token(TokenType.L_BRACKET, None),
+    ']': Token(TokenType.R_BRACKET, None),
+    '{': Token(TokenType.L_BRACE, None),
+    '}': Token(TokenType.R_BRACE, None),
+    ':': Token(TokenType.COLON, None),
+    ',': Token(TokenType.COMMA, None),
+  }
+
+
+flag_symbols = get_symbol_dict().keys()
 
 
 def _switch_token(c: str, i: int) -> tuple:
-  pass
+  tkn, tail = [None, None]
+  if c.isspace():
+    return tkn, i + 1, tail
+  if c in flag_symbols:
+    tkn = get_symbol_dict()[c]
+    return tkn, i + 1, tail
 
 
 def get_tokens(strs: str, index: int=0) -> list:
   char_list = list(strs)
-  length = str_list.__len__()
-
-  flag_symbols = ['[', ']', '{', '}', ':', ',']
+  length = char_list.__len__()
   tokens = []
   _append = tokens.append
   for _ in range(length):
     if index >= length: break
-    char = char_list[index]
-    
-    if char.isspace():
-      index += 1
-      continue
+    tkn, next_index, tail = _switch_token(char_list[index], index)
+    if tkn:
+      tkn.value = char_list[index:tail] if tail else char_list[index]
+      _append(tkn)
+    index = next_index
+  return tokens
 
 
 if __name__ == '__main__':
   from pathlib import Path
 
-  json_path = Path('./sample02.json')
+  json_path = Path('./sample03.json')
   json_str = json_path.read_text(encoding='utf-8')
   t = Token(TokenType.COLON, ':')
+  main = get_tokens(json_str)
 
