b4e4fb3f3b113890cee6c295184bdb65374254cc
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-05-12 15:03:38 +0900

    main

--- sandbox02/__main__.py
+++ sandbox02/__main__.py
@@ -1,12 +1,66 @@
-from pathlib import Path
+from enum import Enum, auto
 
-file_path = Path('./sample02.json')
-str_data = file_path.read_text(encoding='utf-8')
 
-a_s = '-1.1'
-try:
-  a_n = int(a_s)
-except Exception as e:
-  a_n = float(a_s)
+class TokenType(Enum):
+  NUMBER = auto()  # 数値
+  STRING = auto()  # 文字列
+  BOOLEAN = auto()  # true or false
+  NULL = auto()  # null
 
-aaa = 100
+  L_BRACKET = auto()  # [
+  R_BRACKET = auto()  # ]
+  L_BRACE = auto()  # {
+  R_BRACE = auto()  # }
+  COLON = auto()  # :
+  COMMA = auto()  # ,
+
+
+class Token:
+  def __init__(self, type, value=None):
+    self.type: TokenType = type
+    self.value: str = value
+    self.key: bool = False
+    self.nest: int = None
+    self.indent: int = None
+
+  def __str__(self):
+    return str(self.value)
+
+
+symbols = {
+  '[': Token(TokenType.L_BRACKET, None),
+  ']': Token(TokenType.R_BRACKET, None),
+  '{': Token(TokenType.L_BRACE, None),
+  '}': Token(TokenType.R_BRACE, None),
+  ':': Token(TokenType.COLON, None),
+  ':': Token(TokenType.COMMA, None),
+}
+
+
+def _switch_token(c: str, i: int) -> tuple:
+  pass
+
+
+def get_tokens(strs: str, index: int=0) -> list:
+  char_list = list(strs)
+  length = str_list.__len__()
+
+  flag_symbols = ['[', ']', '{', '}', ':', ',']
+  tokens = []
+  _append = tokens.append
+  for _ in range(length):
+    if index >= length: break
+    char = char_list[index]
+    
+    if char.isspace():
+      index += 1
+      continue
+
+
+if __name__ == '__main__':
+  from pathlib import Path
+
+  json_path = Path('./sample02.json')
+  json_str = json_path.read_text(encoding='utf-8')
+  t = Token(TokenType.COLON, ':')
+
