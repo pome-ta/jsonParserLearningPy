4efa0639cb5beaa6791b7084eb10a429038435c5
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-05-14 23:55:49 +0900

    sample test

--- sandbox02/__main__.py
+++ sandbox02/__main__.py
@@ -282,8 +282,11 @@ def parse(strs: str):
 
 if __name__ == '__main__':
   from pathlib import Path
+  import json
 
   json_path = Path('./sample02.json')
   json_str = json_path.read_text(encoding='utf-8')
-  main = parse(json_str)
+  main_json = parse(json_str)
+  main_sample = json.loads(json_str)
+  print(main_json == main_sample)
 
