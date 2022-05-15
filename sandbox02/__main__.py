41a1b903a35c39715a547df3be0b976040348fd7
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-05-15 14:02:37 +0900

    add sample04.json

--- sandbox02/__main__.py
+++ sandbox02/__main__.py
@@ -285,7 +285,7 @@ if __name__ == '__main__':
   from pathlib import Path
   import json
 
-  json_path = Path('./sample02.json')
+  json_path = Path('./sample04.json')
   json_str = json_path.read_text(encoding='utf-8')
   main_json = parse(json_str)
   main_sample = json.loads(json_str)
