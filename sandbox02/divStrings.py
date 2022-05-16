d92532466b6e94756e6e1338480cb1ae4672e2d7
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-05-17 01:11:04 +0900

    string ....to long

--- sandbox02/divStrings.py
+++ sandbox02/divStrings.py
@@ -3,30 +3,41 @@ def stack_str(string: str):
 
 
 def division_strings(strings: str):
-  div_list = []
+  #div_list = []
   str_obj = ''
   is_open = False
+  pre_escape = False
 
   for char in strings:
+    if is_open and pre_escape:
+      str_obj += char
+      is_open =True
+      pre_escape =False
+      continue
+    
     if char == '"':
-      is_open = True
-      continue
-      
-    if is_open and char == '"':
-      print(str_obj)
-      is_open = False
-      str_obj = ''
-      continue
-
+      str_obj += char
+      if is_open:
+        #print(str_obj)
+        str_obj = ''
+        is_open = False
+      else:
+        is_open = True
+        continue
+    
     if is_open:
       str_obj += char
-
+      if char == '\\':
+        pre_escape = True
+    
+      
 
 if __name__ == '__main__':
   from pathlib import Path
   import cProfile
 
-  json_path = Path('./sample02.json')
+  json_path = Path('./sample04.json')
   json_str = json_path.read_text(encoding='utf-8')
-  json_list = division_strings(json_str)
+  #json_list = division_strings(json_str)
+  cProfile.run('division_strings(json_str)', sort=1)
 
