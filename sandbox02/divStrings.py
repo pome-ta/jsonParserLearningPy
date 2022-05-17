18323b1d4d2cd370b9aa0f1fa219013428c1f80d
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-05-17 16:39:28 +0900

    yield

--- sandbox02/divStrings.py
+++ sandbox02/divStrings.py
@@ -1,5 +1,30 @@
-def stack_str(string: str):
-  is_escape = None
+def gen_strings(strings: str):
+  str_obj = ''
+  is_open = False
+  pre_escape = False
+  for char in strings:
+    if is_open and pre_escape:
+      str_obj += char
+      is_open = True
+      pre_escape = False
+      continue
+
+    if char == '"':
+      #str_obj += char
+
+      if is_open:
+        #str_list.append(''.join(str_obj[1:-1]))
+        yield str_obj
+        str_obj = ''
+        is_open = False
+      else:
+        is_open = True
+        continue
+
+    if is_open:
+      str_obj += char
+      if char == '\\':
+        pre_escape = True
 
 
 def division_strings(strings: str):
@@ -16,9 +41,10 @@ def division_strings(strings: str):
       continue
 
     if char == '"':
-      str_obj += char
+      #str_obj += char
+
       if is_open:
-        str_list.append(''.join(str_obj[1:-1]))
+        str_list.append(str_obj)
         str_obj = ''
         is_open = False
       else:
@@ -37,8 +63,10 @@ if __name__ == '__main__':
   from pathlib import Path
   import cProfile
 
-  json_path = Path('./sample04.json')
+  json_path = Path('./sample01.json')
   json_str = json_path.read_text(encoding='utf-8')
-  #json_list = division_strings(json_str)
-  cProfile.run('division_strings(json_str)', sort=1)
+  json_list = division_strings(json_str)
+  join_gen = gen_strings(json_str)
+  print(list(join_gen))
+  #cProfile.run('division_strings(json_str)', sort=1)
 
