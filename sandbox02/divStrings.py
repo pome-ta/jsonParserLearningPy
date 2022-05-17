502dbfe4306310cd3f0561906b4c63f3502b3183
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-05-18 01:16:46 +0900

    Power is Power

--- sandbox02/divStrings.py
+++ sandbox02/divStrings.py
@@ -13,39 +13,136 @@ todo:
 
 
 def gen_strings(strings: str):
-  str_obj = ''
-  is_open = False
-  pre_escape = False
-  isin_string = False
+  match_numbers = [
+    *(lambda: [str(n) for n in range(10)])(), '.', '-', 'e', 'E'
+  ]  # xxx: `e`, `E` は不要？
+  token_obj = ''
+  is_str = False
+  in_escape = False
+  is_number = False
+  is_true = False
+  is_false = False
+  is_null = False
+
   for char in strings:
     # 属性が何かを確認する
+    if not (is_str) and char.isspace():
+      continue
 
-    if isin_string and is_open and pre_escape:
-      str_obj += char
-      is_open = True
-      pre_escape = False
+    #print(char)
+    if is_str and in_escape:
+      token_obj += char
+      is_str = True
+      in_escape = False
       continue
 
     if char == '"':
-      isin_string = True
-      if is_open:
-        yield str_obj
-        str_obj = ''
-        isin_string = False
-        is_open = False
+      if is_str:
+        yield token_obj
+        token_obj = ''
+        is_str = False
+        in_escape = False
+        is_number = False
+        is_true = False
+        is_false = False
+        is_null = False
       else:
-        is_open = True
-        continue
+        is_str = True
+      continue
 
-    if isin_string and is_open:
-      str_obj += char
+    if is_str:
+      token_obj += char
       if char == '\\':
-        pre_escape = True
+        in_escape = True
       continue
 
     if char in ['[', '{', ':', ',', '}', ']']:
+      if token_obj:
+        yield token_obj
       yield char
+      token_obj = ''
+      is_str = False
+      in_escape = False
+      is_number = False
+      is_true = False
+      is_false = False
+      is_null = False
+      continue
 
+    if not (is_str) and char == 't':
+      is_true = True
+      token_obj += char
+      continue
+
+    if not (is_str) and char == 'f':
+      is_false = True
+      token_obj += char
+      continue
+
+    if not (is_str) and char == 'n':
+      is_null = True
+      token_obj += char
+      continue
+
+    if is_true:
+      token_obj += char
+      if len(token_obj) >= 4:
+        if token_obj == 'true':
+          yield token_obj
+          token_obj = ''
+          is_str = False
+          in_escape = False
+          is_number = False
+          is_true = False
+          is_false = False
+          is_null = False
+          continue
+        else:
+          raise Exception(f'bool error: {token_obj}')
+      continue
+
+    if is_false:
+      token_obj += char
+      if len(token_obj) >= 5:
+        if token_obj == 'false':
+          yield token_obj
+          token_obj = ''
+          is_str = False
+          in_escape = False
+          is_number = False
+          is_true = False
+          is_false = False
+          is_null = False
+          continue
+        else:
+          raise Exception(f'bool error: {token_obj}')
+      continue
+
+    if is_null:
+      token_obj += char
+      if len(token_obj) >= 4:
+        if token_obj == 'null':
+          yield token_obj
+          token_obj = ''
+          is_str = False
+          in_escape = False
+          is_number = False
+          is_true = False
+          is_false = False
+          is_null = False
+          continue
+        else:
+          raise Exception(f'null error: {token_obj}')
+      continue
+
+    if not (is_number) and char in match_numbers:
+      is_number = True
+      token_obj += char
+      continue
+
+    if is_number:
+      token_obj += char
+      continue
 
 def set_string(char: str, str_obj: str, open_flag: bool, escape_flag: bool):
   if open_flag and escape_flag:
@@ -60,6 +157,9 @@ def switch_flag(flag):
 
 
 def division_strings(strings: str):
+  match_numbers = [
+    *(lambda: [str(n) for n in range(10)])(), '.', '-', 'e', 'E'
+  ]  # xxx: `e`, `E` は不要？
   obj_list = []
   token_obj = ''
   is_str = False
@@ -74,6 +174,7 @@ def division_strings(strings: str):
     if not (is_str) and char.isspace():
       continue
 
+    #print(char)
     if is_str and in_escape:
       token_obj += char
       is_str = True
@@ -101,8 +202,10 @@ def division_strings(strings: str):
       continue
 
     if char in ['[', '{', ':', ',', '}', ']']:
+      if token_obj:
+        obj_list.append(token_obj)
       obj_list.append(char)
-      #token_obj = ''
+      token_obj = ''
       is_str = False
       in_escape = False
       is_number = False
@@ -111,21 +214,21 @@ def division_strings(strings: str):
       is_null = False
       continue
 
-    if not(is_str) and char == 't':
+    if not (is_str) and char == 't':
       is_true = True
       token_obj += char
       continue
-      
-    if not(is_str) and char == 'f':
+
+    if not (is_str) and char == 'f':
       is_false = True
       token_obj += char
       continue
-      
-    if not(is_str) and char == 'n':
+
+    if not (is_str) and char == 'n':
       is_null = True
       token_obj += char
       continue
-      
+
     if is_true:
       token_obj += char
       if len(token_obj) >= 4:
@@ -142,7 +245,7 @@ def division_strings(strings: str):
         else:
           raise Exception(f'bool error: {token_obj}')
       continue
-        
+
     if is_false:
       token_obj += char
       if len(token_obj) >= 5:
@@ -159,7 +262,7 @@ def division_strings(strings: str):
         else:
           raise Exception(f'bool error: {token_obj}')
       continue
-      
+
     if is_null:
       token_obj += char
       if len(token_obj) >= 4:
@@ -176,8 +279,15 @@ def division_strings(strings: str):
         else:
           raise Exception(f'null error: {token_obj}')
       continue
-    
-      
+
+    if not (is_number) and char in match_numbers:
+      is_number = True
+      token_obj += char
+      continue
+
+    if is_number:
+      token_obj += char
+      continue
 
   return obj_list
 
@@ -186,13 +296,15 @@ if __name__ == '__main__':
   from pathlib import Path
   import cProfile
 
-  json_path = Path('./sample01.json')
+  json_path = Path('./sample04.json')
   json_str = json_path.read_text(encoding='utf-8')
+  '''
   json_list = division_strings(json_str)
   join_gen = gen_strings(json_str)
   join_gen_list = list(join_gen)
   print(join_gen_list == json_list)
+  '''
 
   #cProfile.run('division_strings(json_str)', sort=1)
-  #cProfile.run('gen_strings(json_str)', sort=1)
+  cProfile.run('gen_strings(json_str)', sort=1)
 
